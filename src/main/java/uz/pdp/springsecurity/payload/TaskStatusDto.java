f2d8307a4ff78e44ac60c33ea34af6915a339c13
yoqubdev <yyboboraximov@gmail.com>
2023-03-23 18:21:08 +0500

    task

---
+++ src/main/java/uz/pdp/springsecurity/payload/TaskStatusDto.java
@@ -0,0 +1,18 @@
+package uz.pdp.springsecurity.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TaskStatusDto {
+    private UUID id;
+    private String name;
+    private String color;
+    private UUID businessId;
+}
