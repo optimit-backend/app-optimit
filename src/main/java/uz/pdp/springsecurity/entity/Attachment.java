e9f2ddec3409a983323ea9f1e629d5821c1057c1
samir <sharipovsamir480@gmail.com>
2023-03-29 20:01:40 +0500

    djaf

--- src/main/java/uz/pdp/springsecurity/entity/Attachment.java
+++ src/main/java/uz/pdp/springsecurity/entity/Attachment.java
@@ -9,14 +9,10 @@ import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import java.util.Objects;
 
-@Entity
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Getter
-@Setter
-@ToString
-@RequiredArgsConstructor
-@JsonIgnoreProperties(value={"hibernateLazyInitializer","handler","fieldHandler"})
+@Entity
 public class Attachment extends AbsEntity {
 
     private String fileOriginalName;
@@ -29,24 +25,4 @@ public class Attachment extends AbsEntity {
 
     @ManyToOne
     private Branch branch;
-
-    public Attachment(String fileOriginalName, long size, String contentType) {
-        this.fileOriginalName = fileOriginalName;
-        this.size = size;
-        this.contentType = contentType;
-        
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
-        Attachment that = (Attachment) o;
-        return getId() != null && Objects.equals(getId(), that.getId());
-    }
-
-    @Override
-    public int hashCode() {
-        return getClass().hashCode();
-    }
 }
