c377fd2dec3b96cb985557c0936e66b831484887
samir <sharipovsamir480@gmail.com>
2023-03-29 17:39:43 +0500

    comitda e

--- src/main/java/uz/pdp/springsecurity/entity/Attachment.java
+++ src/main/java/uz/pdp/springsecurity/entity/Attachment.java
@@ -1,17 +1,21 @@
 package uz.pdp.springsecurity.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.Hibernate;
 import uz.pdp.springsecurity.entity.template.AbsEntity;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import java.util.Objects;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @JsonIgnoreProperties(value={"hibernateLazyInitializer","handler","fieldHandler"})
 public class Attachment extends AbsEntity {
 
@@ -31,5 +35,18 @@ public class Attachment extends AbsEntity {
         this.size = size;
         this.contentType = contentType;
         
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Attachment that = (Attachment) o;
+        return getId() != null && Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
     }
 }
