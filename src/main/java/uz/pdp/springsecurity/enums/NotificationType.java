947ea10a906fbf877e060918233331fb52fe38ae
samir <sharipovsamir480@gmail.com>
2023-04-07 18:46:43 +0500

    notification

--- src/main/java/uz/pdp/springsecurity/enums/NotificationType.java
+++ src/main/java/uz/pdp/springsecurity/enums/NotificationType.java
@@ -3,5 +3,6 @@ package uz.pdp.springsecurity.enums;
 public enum NotificationType {
     NEW_BUSINESS,
     NEW_LID,
-    NEW_TASK
+    NEW_TASK,
+    NEW_PROJECT
 }
--- src/main/java/uz/pdp/springsecurity/service/ProjectService.java
+++ src/main/java/uz/pdp/springsecurity/service/ProjectService.java
@@ -1,11 +1,13 @@
 package uz.pdp.springsecurity.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import uz.pdp.springsecurity.entity.*;
+import uz.pdp.springsecurity.enums.NotificationType;
 import uz.pdp.springsecurity.payload.ApiResponse;
 import uz.pdp.springsecurity.payload.ProjectDto;
 import uz.pdp.springsecurity.repository.*;
@@ -14,6 +16,7 @@ import java.sql.Timestamp;
 import java.util.*;
 
 @Service
+@RequiredArgsConstructor
 public class ProjectService {
 
     @Autowired
@@ -50,15 +53,18 @@ public class ProjectService {
 
     @Autowired
     FileDateRepository fileDateRepository;
+    private final NotificationRepository notificationRepository;
+
+
     public ApiResponse add(ProjectDto projectDto) {
         Optional<Branch> optionalBranch = branchRepository.findById(projectDto.getBranchId());
-        if (optionalBranch.isEmpty()){
-            return new ApiResponse("Branch Not Found",false);
+        if (optionalBranch.isEmpty()) {
+            return new ApiResponse("Branch Not Found", false);
         }
 
         Optional<Customer> optionalCustomer = customerRepository.findById(projectDto.getCustomerId());
-        if (optionalCustomer.isEmpty()){
-            return new ApiResponse("Customer Not Found",false);
+        if (optionalCustomer.isEmpty()) {
+            return new ApiResponse("Customer Not Found", false);
         }
 
         Project project = new Project();
@@ -66,7 +72,7 @@ public class ProjectService {
         project.setStartDate(projectDto.getStartDate());
         project.setEndDate(projectDto.getEndDate());
         project.setDeadline(projectDto.getDeadline());
-        if (projectDto.getProjectTypeId()!=null){
+        if (projectDto.getProjectTypeId() != null) {
             Optional<ProjectType> optionalProjectType = projectTypeRepository.findById(projectDto.getProjectTypeId());
             optionalProjectType.ifPresent(project::setProjectType);
         }
@@ -81,10 +87,10 @@ public class ProjectService {
         project.setUsers(userList);
 
         List<FileData> fileDataList = new ArrayList<>();
-        if (projectDto.getFileDateList()!=null){
+        if (projectDto.getFileDateList() != null) {
             for (UUID uuid : projectDto.getFileDateList()) {
                 Optional<FileData> optionalFileData = fileDateRepository.findById(uuid);
-                if (optionalFileData.isPresent()){
+                if (optionalFileData.isPresent()) {
                     FileData fileData = optionalFileData.get();
                     fileDataList.add(fileData);
                 }
@@ -102,13 +108,25 @@ public class ProjectService {
         project.setBranch(optionalBranch.get());
         projectRepository.save(project);
 
-        return new ApiResponse("Added",true);
+        List<User> users = project.getUsers();
+        for (User user : users) {
+            Notification notification = new Notification();
+            notification.setRead(false);
+            notification.setName("Sizga yangi project belgilandi!");
+            notification.setMessage("Project ko'rishingiz mumkin!");
+            notification.setType(NotificationType.NEW_PROJECT);
+            notification.setObjectId(project.getId());
+            notification.setUserTo(user);
+            notificationRepository.save(notification);
+        }
+
+        return new ApiResponse("Added", true);
     }
 
     public ApiResponse edit(UUID id, ProjectDto projectDto) {
         Optional<Project> optionalProject = projectRepository.findById(id);
-        if (optionalProject.isEmpty()){
-            return new ApiResponse("Project Not Found",false);
+        if (optionalProject.isEmpty()) {
+            return new ApiResponse("Project Not Found", false);
         }
         Project project = optionalProject.get();
         project.setName(projectDto.getName());
@@ -132,7 +150,7 @@ public class ProjectService {
         }
         project.setUsers(userList);
 
-        List<FileData> fileDataList= new ArrayList<>();
+        List<FileData> fileDataList = new ArrayList<>();
         if (!projectDto.getFileDateList().isEmpty()) {
             for (UUID uuid : projectDto.getFileDateList()) {
                 Optional<FileData> optionalAttachment = fileDateRepository.findById(uuid);
@@ -153,66 +171,66 @@ public class ProjectService {
         optionalBranch.ifPresent(project::setBranch);
         projectRepository.save(project);
 
-        return new ApiResponse("Edited",true);
+        return new ApiResponse("Edited", true);
     }
 
     public ApiResponse get(UUID id) {
         Optional<Project> optionalProject = projectRepository.findById(id);
-        return optionalProject.map(project -> new ApiResponse("Found",true,project)).orElseGet(() -> new ApiResponse("Project Not Found", false));
+        return optionalProject.map(project -> new ApiResponse("Found", true, project)).orElseGet(() -> new ApiResponse("Project Not Found", false));
     }
 
     public ApiResponse delete(UUID id) {
         boolean exists = projectRepository.existsById(id);
-        if (!exists){
-            return new ApiResponse("Project Not Found",false);
+        if (!exists) {
+            return new ApiResponse("Project Not Found", false);
         }
         projectRepository.deleteById(id);
-        return new ApiResponse("Deleted",true);
+        return new ApiResponse("Deleted", true);
     }
 
-    public ApiResponse  getAllByBranchId(UUID branchId, int page, int size, UUID typeId, UUID stageId, Date startDate,Date endDate) {
-        Pageable pageable = PageRequest.of(page,size);
+    public ApiResponse getAllByBranchId(UUID branchId, int page, int size, UUID typeId, UUID stageId, Date startDate, Date endDate) {
+        Pageable pageable = PageRequest.of(page, size);
         Page<Project> projectList = null;
         Timestamp start = null;
         Timestamp end = null;
         boolean checkingDate = false;
-        if (startDate != null && endDate != null){
+        if (startDate != null && endDate != null) {
             start = new Timestamp(startDate.getTime());
             end = new Timestamp(endDate.getTime());
             checkingDate = true;
         }
 
         boolean checkingType = false;
-        if (typeId != null){
+        if (typeId != null) {
             checkingType = true;
         }
 
         boolean checkingStage = false;
-        if (stageId !=null){
+        if (stageId != null) {
             checkingStage = true;
         }
-        if (page == 0 && size == 0 && !checkingStage && !checkingDate && !checkingType){
+        if (page == 0 && size == 0 && !checkingStage && !checkingDate && !checkingType) {
             List<Project> projects = projectRepository.findAllByBranch_Id(branchId);
-            if (projects.isEmpty()){
-                return new ApiResponse("Not Found",false);
+            if (projects.isEmpty()) {
+                return new ApiResponse("Not Found", false);
             }
-            return new ApiResponse("Found",true,projects);
+            return new ApiResponse("Found", true, projects);
         } else if (!checkingStage && !checkingDate && !checkingType) {
             projectList = projectRepository.findAllByBranchId(branchId, pageable);
         } else if (checkingStage && !checkingDate && !checkingType) {
-            projectList = projectRepository.findAllByBranchIdAndStageId(branchId,stageId,pageable);
+            projectList = projectRepository.findAllByBranchIdAndStageId(branchId, stageId, pageable);
         } else if (checkingStage && checkingDate && !checkingType) {
-            projectList = projectRepository.findAllByBranchIdAndStageIdAndCreatedAtBetween(branchId,stageId,start,end,pageable);
+            projectList = projectRepository.findAllByBranchIdAndStageIdAndCreatedAtBetween(branchId, stageId, start, end, pageable);
         } else if (checkingStage && checkingDate && checkingDate) {
-            projectList = projectRepository.findAllByBranchIdAndStageIdAndProjectTypeIdAndCreatedAtBetween(branchId,stageId,typeId,start,end,pageable);
-        } else if (checkingStage && checkingType && !checkingDate ) {
-            projectList = projectRepository.findAllByBranchIdAndStageIdAndProjectTypeId(branchId,stageId,typeId,pageable);
+            projectList = projectRepository.findAllByBranchIdAndStageIdAndProjectTypeIdAndCreatedAtBetween(branchId, stageId, typeId, start, end, pageable);
+        } else if (checkingStage && checkingType && !checkingDate) {
+            projectList = projectRepository.findAllByBranchIdAndStageIdAndProjectTypeId(branchId, stageId, typeId, pageable);
         } else if (checkingDate) {
-            projectList = projectRepository.findAllByBranchIdAndCreatedAtBetween(branchId,start,end,pageable);
+            projectList = projectRepository.findAllByBranchIdAndCreatedAtBetween(branchId, start, end, pageable);
         }
         assert projectList != null;
-        if (projectList.isEmpty()){
-            return new ApiResponse("Project Not Found",false);
+        if (projectList.isEmpty()) {
+            return new ApiResponse("Project Not Found", false);
         }
 
 
@@ -225,15 +243,15 @@ public class ProjectService {
                 project.setProcess(process);
             }
         }
-        return new ApiResponse("Found",true,projectList);
+        return new ApiResponse("Found", true, projectList);
     }
 
     public ApiResponse findByStageId(UUID statusId) {
         List<Project> projects = projectRepository.findAllByStageId(statusId);
-        if (projects.isEmpty()){
-            return new ApiResponse("Projects Not Found",false);
+        if (projects.isEmpty()) {
+            return new ApiResponse("Projects Not Found", false);
         }
-        return new ApiResponse("Found",true,projects);
+        return new ApiResponse("Found", true, projects);
     }
 
 
