d89266990ae2c40d2a6eaae8414362b76b0fab24
yoqubdev <yyboboraximov@gmail.com>
2023-04-07 18:33:45 +0500

    commit 41

--- src/main/java/uz/pdp/springsecurity/repository/ProjectRepository.java
+++ src/main/java/uz/pdp/springsecurity/repository/ProjectRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import uz.pdp.springsecurity.entity.Attachment;
 import uz.pdp.springsecurity.entity.Project;
 import uz.pdp.springsecurity.entity.ProjectType;
+import uz.pdp.springsecurity.payload.ApiResponse;
 
 import java.sql.Timestamp;
 import java.util.List;
@@ -15,10 +16,16 @@ import java.util.UUID;
 public interface ProjectRepository extends JpaRepository<Project, UUID> {
     List<Project> findAllByStageId(UUID projectStatus_id);
     Page<Project> findAllByBranchId(UUID branchId, Pageable pageable);
-    Page<Project> findAllByBranchIdAndStageId(UUID branchId, UUID stageId, Pageable pageable);
     Page<Project> findAllByBranchIdAndStageIdAndCreatedAtBetween(UUID branch_id, UUID stage_id, Timestamp createdAt, Timestamp createdAt2, Pageable pageable);
-    Page<Project> findAllByBranchIdAndCreatedAtBetween(UUID branch_id, Timestamp createdAt, Timestamp createdAt2, Pageable pageable);
-    Page<Project> findAllByBranchIdAndStageIdAndProjectTypeIdAndCreatedAtBetween(UUID branch_id, UUID stage_id, UUID typeId, Timestamp createdAt, Timestamp createdAt2, Pageable pageable);
-    Page<Project> findAllByBranchIdAndStageIdAndProjectTypeId(UUID branch_id, UUID stage_id, UUID typeId, Pageable pageable);
     List<Project> findAllByBranch_Id(UUID branch_id);
+    Page<Project> findAllByBranchIdAndProjectTypeIdAndStageIdAndCustomerIdAndCreatedAtBetween(UUID branchId, UUID typeId, UUID stageId, UUID customerId, Timestamp start, Timestamp end,Pageable pageable);
+    Page<Project> findAllByBranchIdAndProjectTypeIdAndStageIdAndCustomerId(UUID branch_id, UUID projectType_id, UUID stage_id, UUID customer_id, Pageable pageable);
+    Page<Project> findAllByBranchIdAndProjectTypeIdAndCustomerIdAndCreatedAtBetween(UUID branchId, UUID typeId, UUID customerId, Timestamp start, Timestamp end, Pageable pageable);
+    Page<Project> findAllByBranchIdAndProjectTypeIdAndStageIdAndCreatedAtBetween(UUID branchId, UUID typeId, UUID stageId, Timestamp start, Timestamp end, Pageable pageable);
+    Page<Project> findAllByBranchIdAndStageIdAndCustomerIdAndCreatedAtBetween(UUID branchId, UUID stageId, UUID customerId, Timestamp start, Timestamp end, Pageable pageable);
+    Page<Project> findAllByBranchIdAndProjectTypeIdAndCustomerId(UUID branchId, UUID typeId, UUID customerId, Pageable pageable);
+    Page<Project> findAllByBranchIdAndProjectTypeIdAndStageId(UUID branchId, UUID typeId, UUID stageId, Pageable pageable);
+    Page<Project> findAllByBranchIdAndProjectTypeIdAndCreatedAtBetween(UUID branchId, UUID typeId, Timestamp start, Timestamp end, Pageable pageable);
+    Page<Project> findAllByBranchIdAndStageIdAndCustomerId(UUID branchId, UUID stageId, UUID customerId, Pageable pageable);
+    Page<Project> findAllByBranchIdAndCustomerIdAndCreatedAtBetween(UUID branchId, UUID customerId, Timestamp start, Timestamp end, Pageable pageable);
 }
--- src/main/java/uz/pdp/springsecurity/repository/TaskTypeRepository.java
+++ src/main/java/uz/pdp/springsecurity/repository/TaskTypeRepository.java
@@ -1,5 +1,7 @@
 package uz.pdp.springsecurity.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import uz.pdp.springsecurity.entity.TaskType;
 
@@ -8,4 +10,5 @@ import java.util.UUID;
 
 public interface TaskTypeRepository extends JpaRepository<TaskType, UUID> {
     List<TaskType> findAllByBranchId(UUID branchId);
+    Page<TaskType> findAllByBranch_Id(UUID branch_id, Pageable pageable);
 }
