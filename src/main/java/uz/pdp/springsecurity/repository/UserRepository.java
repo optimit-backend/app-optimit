1b9300d3d73e0fe200af0f9c59de76a34ae5b4f6
yoqubdev <yyboboraximov@gmail.com>
2023-04-13 12:59:56 +0500

    commit 53

--- src/main/java/uz/pdp/springsecurity/repository/UserRepository.java
+++ src/main/java/uz/pdp/springsecurity/repository/UserRepository.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
-    boolean existsById(UUID userId);
+    boolean existsById(UUID id);
     boolean existsByUsernameIgnoreCase(String username);
     Optional<User> findByUsername(String username);
     List<User> findAllByRole_Id(UUID role_id);
