c257861888cc9ac65debf338a7779d70ae1b8d4b
samir <sharipovsamir480@gmail.com>
2023-04-14 16:30:55 +0500

    14.04.2023 1.2

--- src/main/java/uz/pdp/springsecurity/repository/ProjectRepository.java
+++ src/main/java/uz/pdp/springsecurity/repository/ProjectRepository.java
@@ -16,51 +16,51 @@ import java.util.UUID;
 
 public interface ProjectRepository extends JpaRepository<Project, UUID> {
 
-    List<Project> findAllByStageId(UUID projectStatus_id);
     boolean existsByProjectTypeId(UUID projectType_id);
 
     Page<Project> findAllByBranchId(UUID branchId, Pageable pageable);
+
+    List<Project> findAllByBranch_Id(UUID branch_id);
+
+//    List<Project> findAllByStageId(UUID projectStatus_id);
 //    Page<Project> findAllByBranchIdAndCustomerId(UUID branch_id, UUID customer_id, Pageable pageable);
 //    Page<Project> findAllByBranchIdAndProjectTypeId(UUID branch_id, UUID stage_id, Pageable pageable);
 //    Page<Project> findAllByBranchIdAndCreatedAtBetween(UUID branch_id, Timestamp createdAt, Timestamp createdAt2, Pageable pageable);
-    List<Project> findAllByBranch_Id(UUID branch_id);
 //    Page<Project> findAllByBranchIdAndProjectTypeIdAndCustomerId(UUID branch_id, UUID projectType_id, UUID stage_id, UUID customer_id, Pageable pageable);
 //    Page<Project> findAllByBranchIdAndProjectTypeIdAndCustomerIdAndCreatedAtBetween(UUID branchId, UUID typeId, UUID customerId, Timestamp start, Timestamp end, Pageable pageable);
 //    Page<Project> findAllByBranchIdAndProjectTypeIdAndCreatedAtBetween(UUID branchId, UUID typeId, Timestamp start, Timestamp end, Pageable pageable);
 //    Page<Project> findAllByBranchIdAndProjectTypeIdAndCustomerId(UUID branchId, UUID typeId, UUID customerId, Pageable pageable);
 //    Page<Project> findAllByBranchIdAndCustomerIdAndCreatedAtBetween(UUID branchId, UUID customerId, Timestamp start, Timestamp end, Pageable pageable);
-
-
-    Page<Project> findAllByBranchIdAndStageIdAndCreatedAtBetween(UUID branch_id, UUID stage_id, Timestamp createdAt, Timestamp createdAt2, Pageable pageable);
-
-    Page<Project> findAllByBranchIdAndCustomerId(UUID branch_id, UUID customer_id, Pageable pageable);
-
-    Page<Project> findAllByBranchIdAndStageId(UUID branch_id, UUID stage_id, Pageable pageable);
-
-    Page<Project> findAllByBranchIdAndProjectTypeId(UUID branch_id, UUID stage_id, Pageable pageable);
-
-    Page<Project> findAllByBranchIdAndCreatedAtBetween(UUID branch_id, Timestamp createdAt, Timestamp createdAt2, Pageable pageable);
-
-
-    Page<Project> findAllByBranchIdAndProjectTypeIdAndStageIdAndCustomerIdAndCreatedAtBetween(UUID branchId, UUID typeId, UUID stageId, UUID customerId, Timestamp start, Timestamp end, Pageable pageable);
-
-    Page<Project> findAllByBranchIdAndProjectTypeIdAndStageIdAndCustomerId(UUID branch_id, UUID projectType_id, UUID stage_id, UUID customer_id, Pageable pageable);
-
-    Page<Project> findAllByBranchIdAndProjectTypeIdAndCustomerIdAndCreatedAtBetween(UUID branchId, UUID typeId, UUID customerId, Timestamp start, Timestamp end, Pageable pageable);
-
-    Page<Project> findAllByBranchIdAndProjectTypeIdAndStageIdAndCreatedAtBetween(UUID branchId, UUID typeId, UUID stageId, Timestamp start, Timestamp end, Pageable pageable);
-
-    Page<Project> findAllByBranchIdAndStageIdAndCustomerIdAndCreatedAtBetween(UUID branchId, UUID stageId, UUID customerId, Timestamp start, Timestamp end, Pageable pageable);
-
-    Page<Project> findAllByBranchIdAndProjectTypeIdAndCustomerId(UUID branchId, UUID typeId, UUID customerId, Pageable pageable);
-
-    Page<Project> findAllByBranchIdAndProjectTypeIdAndStageId(UUID branchId, UUID typeId, UUID stageId, Pageable pageable);
-
-    Page<Project> findAllByBranchIdAndProjectTypeIdAndCreatedAtBetween(UUID branchId, UUID typeId, Timestamp start, Timestamp end, Pageable pageable);
-
-    Page<Project> findAllByBranchIdAndStageIdAndCustomerId(UUID branchId, UUID stageId, UUID customerId, Pageable pageable);
-
-    Page<Project> findAllByBranchIdAndCustomerIdAndCreatedAtBetween(UUID branchId, UUID customerId, Timestamp start, Timestamp end, Pageable pageable);
+//    Page<Project> findAllByBranchIdAndStageIdAndCreatedAtBetween(UUID branch_id, UUID stage_id, Timestamp createdAt, Timestamp createdAt2, Pageable pageable);
+//
+//    Page<Project> findAllByBranchIdAndCustomerId(UUID branch_id, UUID customer_id, Pageable pageable);
+//
+//    Page<Project> findAllByBranchIdAndStageId(UUID branch_id, UUID stage_id, Pageable pageable);
+//
+//    Page<Project> findAllByBranchIdAndProjectTypeId(UUID branch_id, UUID stage_id, Pageable pageable);
+//
+//    Page<Project> findAllByBranchIdAndCreatedAtBetween(UUID branch_id, Timestamp createdAt, Timestamp createdAt2, Pageable pageable);
+//
+//
+//    Page<Project> findAllByBranchIdAndProjectTypeIdAndStageIdAndCustomerIdAndCreatedAtBetween(UUID branchId, UUID typeId, UUID stageId, UUID customerId, Timestamp start, Timestamp end, Pageable pageable);
+//
+//    Page<Project> findAllByBranchIdAndProjectTypeIdAndStageIdAndCustomerId(UUID branch_id, UUID projectType_id, UUID stage_id, UUID customer_id, Pageable pageable);
+//
+//    Page<Project> findAllByBranchIdAndProjectTypeIdAndCustomerIdAndCreatedAtBetween(UUID branchId, UUID typeId, UUID customerId, Timestamp start, Timestamp end, Pageable pageable);
+//
+//    Page<Project> findAllByBranchIdAndProjectTypeIdAndStageIdAndCreatedAtBetween(UUID branchId, UUID typeId, UUID stageId, Timestamp start, Timestamp end, Pageable pageable);
+//
+//    Page<Project> findAllByBranchIdAndStageIdAndCustomerIdAndCreatedAtBetween(UUID branchId, UUID stageId, UUID customerId, Timestamp start, Timestamp end, Pageable pageable);
+//
+//    Page<Project> findAllByBranchIdAndProjectTypeIdAndCustomerId(UUID branchId, UUID typeId, UUID customerId, Pageable pageable);
+//
+//    Page<Project> findAllByBranchIdAndProjectTypeIdAndStageId(UUID branchId, UUID typeId, UUID stageId, Pageable pageable);
+//
+//    Page<Project> findAllByBranchIdAndProjectTypeIdAndCreatedAtBetween(UUID branchId, UUID typeId, Timestamp start, Timestamp end, Pageable pageable);
+//
+//    Page<Project> findAllByBranchIdAndStageIdAndCustomerId(UUID branchId, UUID stageId, UUID customerId, Pageable pageable);
+//
+//    Page<Project> findAllByBranchIdAndCustomerIdAndCreatedAtBetween(UUID branchId, UUID customerId, Timestamp start, Timestamp end, Pageable pageable);
 
     List<Project> findAllByUsersId(UUID users_id);
 
