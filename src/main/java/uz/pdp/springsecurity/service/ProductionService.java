8d062eff46c69b38a12b7533f701827ad8ba82e3
Anvarprimov08 <anvarprimov07@gmail.com>
2023-05-09 16:42:00 +0500

    2023.5.9 - 2

--- src/main/java/uz/pdp/springsecurity/service/ProductionService.java
+++ src/main/java/uz/pdp/springsecurity/service/ProductionService.java
@@ -215,4 +215,25 @@ public class ProductionService {
 //        prizeService.addForTask(task);
         return new ApiResponse("SUCCESS", true);
     }
+
+    public ApiResponse getAll(UUID branchId) {
+        Optional<Branch> optionalBranch = branchRepository.findById(branchId);
+        if (optionalBranch.isEmpty()) return new ApiResponse("NOT FOUND BRANCH", false);
+        List<Production> productionList = productionRepository.findAllByBranchId(branchId);
+        if (productionList.isEmpty())return new ApiResponse("NOT FOUND", false);
+        return new ApiResponse(true, productionList);
+    }
+
+    public ApiResponse getOne(UUID productionId) {
+        Optional<Production> optionalProduction = productionRepository.findById(productionId);
+        if (optionalProduction.isEmpty())return new ApiResponse("NOT FOUND", false);
+        Production production = optionalProduction.get();
+        List<ContentProduct> contentProductList = contentProductRepository.findAllByProductionId(productionId);
+        if (contentProductList.isEmpty()) return new ApiResponse("NOT FOUND CONTENT PRODUCTS", false);
+        GetOneContentProductionDto getOneContentProductionDto = new GetOneContentProductionDto(
+                production,
+                contentProductList
+        );
+        return new ApiResponse(true, getOneContentProductionDto);
+    }
 }
