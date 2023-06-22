550b7ecc3da9a2d66e04052faec15b3bf4a2272e
yoqubdev <yyboboraximov@gmail.com>
2023-06-22 18:45:51 +0500

    history

--- src/main/java/uz/pdp/springsecurity/component/DataLoader.java
+++ src/main/java/uz/pdp/springsecurity/component/DataLoader.java
@@ -66,7 +66,7 @@ public class DataLoader implements CommandLineRunner {
                         "test tariff",
                         "test uchun",
                         List.of(permissions),
-                        0,
+                        10,
                         0,
                         0,
                         0,
